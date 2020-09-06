09000d6c9442ccee084aff6185efc0192b4f4362
Nitin Marale <66687755+nitinmarale@users.noreply.github.com>
2020-09-06 13:33:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
  */
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
-  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=metric`;
   const weatherPromise  = fetch(FULL_URL);
   return weatherPromise.then((response) => {
     return response.json();
